1077642a3773b3c9c327e12a9b6c1a4da3343e63
quyhungfmt <ngquyhuq3128@gmail.com>
2023-08-25 21:31:14 +0700

    sssl

--- src/Screen/Login/index.js
+++ src/Screen/Login/index.js
@@ -1,15 +1,9 @@
-var lg = document.querySelector(".boxLogin");
-var rgt = document.querySelector(".boxRegister");
-var btL = document.getElementById("btHlogin");
-var btR = document.getElementById("btHregister");
+
 var ifagree = document.getElementById("ifagree");
-
-
-var error_email = document.getElementById("inputEmail")
-var error_pass = document.getElementById("inputPass")
-var error_email2 = document.getElementById("inputEmail2")
-var error_pass2 = document.getElementById("inputPass2")
-var error_pass2A = document.getElementById("passAgain")
+var error_email = document.getElementById("lineEmail")
+var error_pass = document.getElementById("linePass")
+var titleE = document.getElementById("titleE")
+var titleP = document.getElementById("titleP")
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyB-hS9CcEIy5304p_ZDqk5KPoYdrVnfbB4",
@@ -30,67 +24,52 @@ function style_error (x) {
         case 1:
             // email error
             document.getElementById("errorEmail").innerHTML="Vui Lòng Nhập Email";
-            error_email.style.borderBottomColor = "red"
+            error_email.style.backgroundColor = "red"
             break;
         case 2:
             // email succsess
             document.getElementById("errorEmail").innerHTML="";
-            error_email.style.borderBottomColor = "green"
+            error_email.style.backgroundColor = "#45f3ff"
             break;
         case 3:
             // pass error
             document.getElementById("errorPass").innerHTML="Vui Lòng Nhập Mật Khẩu" 
-            error_pass.style.borderBottomColor = "red"
+            error_pass.style.backgroundColor = "red"
             break;
         case 4:
             // pass error length
             document.getElementById("errorPass").innerHTML="Mật Khẩu Phải Có Hơn 6 Kí Tự"
-            error_pass.style.borderBottomColor = "red"
+            error_pass.style.backgroundColor = "red"
             break;
         case 5:
             // pass succsess
             document.getElementById("errorPass").innerHTML="";
-            error_pass.style.borderBottomColor = "green"
-            break;
-        case 6:
-            // email register error
-            document.getElementById("errorEmail2").innerHTML="Vui Lòng Nhập Email";
-            error_email2.style.borderBottomColor = "red"
-            break;
-        case 7:
-            // email register succsess
-            document.getElementById("errorEmail2").innerHTML="";
-            error_email2.style.borderBottomColor = "green"
-            break;
-        case 8:
-            // pass register error 
-            document.getElementById("errorPass2").innerHTML="Vui Lòng Nhập Mật Khẩu" 
-            error_pass2.style.borderBottomColor = "red"
-            break;
-        case 9:
-            // pass register error length
-            document.getElementById("errorPass2").innerHTML="Mật Khẩu Phải Có Hơn 6 Kí Tự" 
-            error_pass2.style.borderBottomColor = "red"
-            break; 
-        case 10:
-            // pass register succsess
-            document.getElementById("errorPass2").innerHTML="";
-            error_pass2.style.borderBottomColor = "green"
-            break;
-        case 11:
-            document.getElementById("errorpass2Again").innerHTML="Vui Lòng Nhập Lại Mật Khẩu";
-            error_pass2A.style.borderBottomColor ="red"
-            break;
-        case 12:
-            document.getElementById("errorpass2Again").innerHTML="Mật Khẩu Không Khớp";
-            error_pass2A.style.borderBottomColor ="red"
-            break;
-        case 13:
-            document.getElementById("errorpass2Again").innerHTML="";
-            error_pass2A.style.borderBottomColor ="green"    
+            error_pass.style.backgroundColor = "#45f3ff"
             break;
     }
 }
+const showTitle = (title,ishow) => {
+    if(ishow)
+    {
+        title.style.transform = "translate(-320px,15px)"
+        title.style.transition = ".5s"
+        title.style.color = "#45f3ff"
+    }
+    else {
+        title.style.transform = "translate(-320px,39px)"
+        title.style.transition = ".5s"    
+        title.style.color = "transparent"
+    }
+}
+const showtitleE = () => {
+    var emailvalue = document.getElementById("inputEmail").value;
+    emailvalue != "" ? showTitle(titleE,1) : showTitle(titleE,0)
+}
+const showtitleP = () => {
+    var passvalue = document.getElementById("inputPass").value;
+    passvalue != "" ? showTitle(titleP,1) : showTitle(titleP,0)
+}
+
 function validateEmail () {
     var emailvalue = document.getElementById("inputEmail").value;
     emailvalue == "" ? style_error(1) : style_error(2)
@@ -144,78 +123,6 @@ function validationLogin () {
         return false
     }
 }
-function validateEmail2 () {
-    var emailvalue2 = document.getElementById("inputEmail2").value;
-    emailvalue2 == ""? style_error(6) : style_error(7)
-}
-function validatePass2 () {
-    var passvalue2 = document.getElementById("inputPass2").value;
-    if(passvalue2 == "") style_error(8)
-    else if(passvalue2.length <6 && passvalue2.length >0) style_error(9)
-    else if(passvalue2.length >=6) style_error(10);
-}
-function validatePassAgain() {
-    var passvalue2 = document.getElementById("inputPass2").value;
-    var pass2Again2 = document.getElementById("passAgain").value;
-    console.log(passvalue2)
-    console.log(pass2Again2)
-    if(pass2Again2.length == 0)
-    style_error(11) 
-    else if(pass2Again2 != passvalue2)
-    {
-    style_error(12)
-    }
-    else
-    style_error(13)
-}
-const loginss = () => {
-    var emailvalue = document.getElementById("inputEmail2").value;
-    var passvalue2 = document.getElementById("inputPass2").value;
-    firebase.auth().createUserWithEmailAndPassword(emailvalue, passvalue2)
-    .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    console.log(user);
-    // ...
-    return false
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-    console.log(errorCode)
-    return false
-    // ..
-  });
-  alert('true')
-}
-
-function validationRegister () {
-    var emailvalue = document.getElementById("inputEmail2").value;
-    var passvalue2 = document.getElementById("inputPass2").value;
-    var pass2Again = document.getElementById("passAgain").value;
-    let agree = document.getElementById("agree");
-    console.log(emailvalue !="" && passvalue2 !="" && pass2Again==passvalue2)
-    if(emailvalue == "" && passvalue2 =="" && pass2Again =="")
-    {style_error(6)
-    style_error(8)
-    style_error(11)
-    return false}
-    else if(emailvalue !="" && passvalue2 !="" && pass2Again==passvalue2 &&agree.checked){
-        loginss();
-        return false
-    }
-    else if(agree)
-    {
-        ifagree.style.color ="red"
-        return false
-    }
-    else {
-        return false
-    }
-    
-}
-
 
 
 
@@ -226,27 +133,3 @@ auth/user-not-found: Không tìm thấy người dùng với địa chỉ email 
 auth/wrong-password: Mật khẩu không đúng.
 auth/network-request-failed: Lỗi kết nối mạng1.
 */
-
-
-
-
-
-
-
-
-function register() {
-    // rgt.classList.add('is')
-    // lg.classList.add('is')
-    btR.style.background = "linear-gradient(to right, yellow, red)";
-    btL.style.background = "transparent"
-    console.log('regisre');
-    
-}
-function login() {
-    // rgt.classList.remove('is')
-    // lg.classList.remove('is')
-    // bt.classList.remove('register')
-    btL.style.background = "linear-gradient(to right, yellow, red)";
-    btR.style.background = "transparent"
-    console.log('login');
-}
